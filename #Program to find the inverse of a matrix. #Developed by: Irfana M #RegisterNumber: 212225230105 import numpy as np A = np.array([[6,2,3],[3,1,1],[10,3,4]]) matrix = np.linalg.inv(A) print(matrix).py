7aadd9826db02db66f1437d2d85baf7043fa0ac1
zibairfana08-AIDS <zibairfana08@gmail.com>
2026-02-04 21:33:45 +0530

    Create #Program to find the inverse of a matrix. #Developed by: Irfana M #RegisterNumber: 212225230105 import numpy as np A = np.array([[6,2,3],[3,1,1],[10,3,4]]) matrix = np.linalg.inv(A) print(matrix).py

---
+++ #Program to find the inverse of a matrix. #Developed by: Irfana M #RegisterNumber: 212225230105 import numpy as np A = np.array([[6,2,3],[3,1,1],[10,3,4]]) matrix = np.linalg.inv(A) print(matrix).py
@@ -0,0 +1,7 @@
+#Program to find the inverse of a matrix.
+#Developed by: Irfana M
+#RegisterNumber: 212225230105
+import numpy as np
+A = np.array([[6,2,3],[3,1,1],[10,3,4]])
+matrix = np.linalg.inv(A)
+print(matrix)
